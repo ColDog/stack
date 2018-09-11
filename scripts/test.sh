2ce51723021bacb7dff56efa38a878db6e0e7492
Colin Walker <colinwalker270@gmail.com>
2018-09-10 20:40:07 -0700

    Remove scripts package.

--- scripts/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-modules=$(find -mindepth 2 -name *.tf -printf '%P\n' | xargs -I % dirname %)
-
-(terraform validate . && echo "√ stack") || exit 1
-
-for m in $modules; do
-  (terraform validate $m && echo "√ $m") || exit 1
-done
